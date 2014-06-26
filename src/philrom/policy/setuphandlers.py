e4246f0db5e2ca73d38f1b8d0e9a7f6090e170ba
Manuel Reinhardt <reinhardt@syslab.com>
2014-06-26 16:14:37 +0100

    stubs for custom region, epoch and topic vocabularies, refs #10334

---
+++ src/philrom/policy/setuphandlers.py
@@ -0,0 +1,17 @@
+import pkg_resources
+from Products.CMFCore.utils import getToolByName
+
+
+def importVocabularies(self):
+    if self.readDataFile('philrom.policy_marker.txt') is None:
+        return
+    path_tmpl = '../../../vocabularies/%s'
+    site = self.getSite()
+    pvm = getToolByName(site, 'portal_vocabularies')
+    for (filenamepart, vocabname) in (('area.vdex', 'region_values'),
+                                      ('temi.vdex', 'topic_values'),
+                                      ('periodo.vdex', 'epoch_values'),):
+        pvm.manage_delObjects([vocabname])
+        pvm.invokeFactory('VdexFileVocabulary', vocabname)
+        pvm[vocabname].importXMLBinding(pkg_resources.resource_string(
+            __name__, path_tmpl % filenamepart))
