024eb599739b1575eeb3651e974aff95de8043fc
Wolfgang Thomas <thomas@syslab.com>
2014-11-12 17:01:43 +0100

    added our own manual_translations.py, so that we can make sure certain labels get picked up by the extractor

---
+++ src/philrom/policy/manual_translations.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Dont import this file, it's just for our i18n toolchain
+
+from recensio.policy import recensioMessageFactory as _
+
+_(u"label_journals", default=u"Journals")
+_(u"label_edited_volumes", default=u"Edited Volumes")
