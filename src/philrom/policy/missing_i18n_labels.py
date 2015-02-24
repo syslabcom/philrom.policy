626f16f8aa3f06ab406db6b3160e958d8aca64bf
Manuel Reinhardt <reinhardt@syslab.com>
2015-02-24 10:57:14 +0000

    added missing_i18n_labels, updated recensio.pot, refs #11323

---
+++ src/philrom/policy/missing_i18n_labels.py
@@ -0,0 +1,7 @@
+# Dont import this file, its just for our i18n toolchain
+
+from recensio.policy import recensioMessageFactory as _
+
+_("Article in a Journal or Edited Volume")
+_("Edited Volume")
+_("Journal")
